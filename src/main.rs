f2724a9449ed8cdfc52e452f9f5c169f32806e84
nafiz1001 <nafiz.islam1001@gmail.com>
2021-09-19 13:03:06 -0400

    enumerate processes and modules with enumerator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(non_snake_case)]
+
 use core::{panic, time};
 use std::{
     ffi::{c_void, OsString},
@@ -12,24 +14,34 @@ use std::{
 
 use log::{Level, LevelFilter, Metadata, Record, SetLoggerError};
 use windows_bindings::Windows::Win32::{
-    Foundation::{CloseHandle, HANDLE, HINSTANCE, INVALID_HANDLE_VALUE},
-    System::{Diagnostics::Debug::ReadProcessMemory, Threading::PROCESS_VM_READ},
+    Foundation::{CloseHandle, HANDLE, HINSTANCE, INVALID_HANDLE_VALUE, MAX_PATH},
     System::{
         Diagnostics::ToolHelp::{
-            CreateToolhelp32Snapshot, Module32FirstW, MODULEENTRY32W, TH32CS_SNAPMODULE,
-            TH32CS_SNAPMODULE32,
+            CreateToolhelp32Snapshot, Module32FirstW, Module32NextW, Process32FirstW,
+            Process32NextW, MODULEENTRY32W, TH32CS_SNAPMODULE, TH32CS_SNAPMODULE32,
+            TH32CS_SNAPPROCESS,
         },
         Threading::{OpenProcess, PROCESS_ACCESS_RIGHTS},
+    },
+    System::{
+        Diagnostics::{Debug::ReadProcessMemory, ToolHelp::PROCESSENTRY32W},
+        Threading::PROCESS_VM_READ,
     },
 };
 
 struct Handle(HANDLE);
 
-impl Drop for Handle {
-    fn drop(&mut self) {
+impl Handle {
+    fn close(&self) {
         unsafe {
             CloseHandle(self.0);
         }
+    }
+}
+
+impl Drop for Handle {
+    fn drop(&mut self) {
+        self.close()
     }
 }
 
@@ -39,87 +51,149 @@ impl fmt::Display for Handle {
     }
 }
 
-struct ModuleEntry {
-    _module_id: u32,
-    _process_id: u32,
-    _glblcnt_usage: u32,
-    _proccnt_usage: u32,
-    mod_base_addr: *mut u8,
-    _mod_base_size: u32,
-    _h_module: HINSTANCE,
-    _module_name: String,
-    _exe_path: String,
+fn wide_chars_to_string(wide_chars: &[u16]) -> String {
+    OsString::from_wide(wide_chars)
+        .to_string_lossy()
+        .trim_end_matches(char::from(0))
+        .to_string()
 }
 
-impl ModuleEntry {
-    pub fn from(&module_entry: &MODULEENTRY32W) -> Self {
-        return Self {
-            _module_id: module_entry.th32ModuleID,
-            _process_id: module_entry.th32ProcessID,
-            _glblcnt_usage: module_entry.GlblcntUsage,
-            _proccnt_usage: module_entry.ProccntUsage,
-            mod_base_addr: module_entry.modBaseAddr,
-            _mod_base_size: module_entry.modBaseSize,
-            _h_module: module_entry.hModule,
-            _module_name: OsString::from_wide(&module_entry.szModule[..])
-                .to_string_lossy()
-                .trim_end_matches(char::from(0))
-                .to_string(),
-            _exe_path: OsString::from_wide(&module_entry.szExePath[..])
-                .to_string_lossy()
-                .trim_end_matches(char::from(0))
-                .to_string(),
+struct ProcessEnumerator {
+    handle: Handle,
+    count: usize,
+}
+
+impl ProcessEnumerator {
+    fn new() -> Self {
+        unsafe {
+            let handle = Handle(CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0));
+            if handle.0 == INVALID_HANDLE_VALUE {
+                panic!("CreateToolhelp32Snapshot failed");
+            } else {
+                return Self {
+                    handle: handle,
+                    count: 0,
+                };
+            }
+        }
+    }
+}
+
+impl Iterator for ProcessEnumerator {
+    type Item = PROCESSENTRY32W;
+
+    // next() is the only required method
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut process_entry = PROCESSENTRY32W {
+            dwSize: size_of::<PROCESSENTRY32W>() as u32,
+            cntUsage: 0,
+            th32ProcessID: 0,
+            th32DefaultHeapID: 0,
+            th32ModuleID: 0,
+            cntThreads: 0,
+            th32ParentProcessID: 0,
+            pcPriClassBase: 0,
+            dwFlags: 0,
+            szExeFile: [0u16; MAX_PATH as usize],
         };
+
+        unsafe {
+            if self.count == 0 {
+                if !Process32FirstW(self.handle.0, &mut process_entry).as_bool() {
+                    panic!("Process32FirstW failed");
+                } else {
+                    self.count += 1;
+                    return Some(process_entry);
+                }
+            } else {
+                if !Process32NextW(self.handle.0, &mut process_entry).as_bool() {
+                    return None;
+                } else {
+                    self.count += 1;
+                    return Some(process_entry);
+                }
+            }
+        }
+    }
+}
+
+struct ModuleEnumerator {
+    handle: Handle,
+    count: usize,
+}
+
+impl ModuleEnumerator {
+    fn new(pid: u32) -> Self {
+        unsafe {
+            let handle = Handle(CreateToolhelp32Snapshot(
+                TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32,
+                pid,
+            ));
+            if handle.0 == INVALID_HANDLE_VALUE {
+                panic!("CreateToolhelp32Snapshot failed");
+            } else {
+                return Self {
+                    handle: handle,
+                    count: 0,
+                };
+            }
+        }
+    }
+}
+
+impl Iterator for ModuleEnumerator {
+    type Item = MODULEENTRY32W;
+
+    // next() is the only required method
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut module_entry = MODULEENTRY32W {
+            dwSize: size_of::<MODULEENTRY32W>() as u32,
+            th32ProcessID: 0,
+            th32ModuleID: 0,
+            GlblcntUsage: 0,
+            ProccntUsage: 0,
+            modBaseAddr: null_mut(),
+            modBaseSize: 0,
+            hModule: HINSTANCE(0),
+            szModule: [0u16; 256],
+            szExePath: [0u16; MAX_PATH as usize],
+        };
+
+        unsafe {
+            if self.count == 0 {
+                if !Module32FirstW(self.handle.0, &mut module_entry).as_bool() {
+                    panic!("Process32FirstW failed");
+                } else {
+                    self.count += 1;
+                    return Some(module_entry);
+                }
+            } else {
+                if !Module32NextW(self.handle.0, &mut module_entry).as_bool() {
+                    return None;
+                } else {
+                    self.count += 1;
+                    return Some(module_entry);
+                }
+            }
+        }
     }
 }
 
 struct Process {
     handle: Handle,
-    module: ModuleEntry,
+    module: MODULEENTRY32W,
 }
 
 impl Process {
-    pub fn new(pid: u32, desired_access: PROCESS_ACCESS_RIGHTS) -> Self {
+    fn new(pid: u32, desired_access: PROCESS_ACCESS_RIGHTS) -> Self {
         let handle: Handle;
         unsafe {
             handle = Handle(OpenProcess(desired_access, false, pid));
         }
 
-        let module: ModuleEntry;
+        let module = ModuleEnumerator::new(pid).next().unwrap();
 
-        unsafe {
-            let h_snapshot = Handle(CreateToolhelp32Snapshot(
-                TH32CS_SNAPMODULE32 | TH32CS_SNAPMODULE,
-                pid,
-            ));
-            if h_snapshot.0 == INVALID_HANDLE_VALUE {
-                panic!("CreateToolhelp32Snapshot failed");
-            }
-
-            let mut module_entry = MODULEENTRY32W {
-                dwSize: size_of::<MODULEENTRY32W>() as u32,
-                th32ProcessID: 0,
-                th32ModuleID: 0,
-                GlblcntUsage: 0,
-                ProccntUsage: 0,
-                modBaseAddr: null_mut(),
-                modBaseSize: 0,
-                hModule: HINSTANCE(0),
-                szModule: [0u16; 256],
-                szExePath: [0u16; 260],
-            };
-
-            if Module32FirstW(h_snapshot.0, &mut module_entry).as_bool() {
-                module = ModuleEntry::from(&module_entry);
-            } else {
-                panic!("Module32FirstW failed");
-            }
-        }
-
-        Self {
-            handle: handle,
-            module: module,
-        }
+        Self { handle, module }
     }
 
     fn read_process_memory(&self, range: Range<usize>) -> Vec<u8> {
@@ -129,7 +203,7 @@ impl Process {
         unsafe {
             if !ReadProcessMemory(
                 self.handle.0,
-                (self.module.mod_base_addr as usize + range.start) as *const c_void,
+                (self.module.modBaseAddr as usize + range.start) as *const c_void,
                 buffer.as_mut_ptr() as *mut c_void,
                 buffer.len(),
                 null_mut() as *mut usize,
@@ -171,6 +245,18 @@ pub fn init_log() -> Result<(), SetLoggerError> {
 
 fn main() {
     init_log().expect("could not initialize log");
+
+    let mut processes: Vec<_> = ProcessEnumerator::new().map(|entry| (entry.th32ProcessID, wide_chars_to_string(&entry.szExeFile[..]))).collect();
+    processes
+        .sort_by(|a, b| a.1.to_lowercase().cmp(&b.1.to_lowercase()));
+
+    for (pid, name) in processes {
+        println!(
+            "{}\t{}",
+            pid,
+            name,
+        );
+    }
 
     print!("Enter Process ID: ");
     io::stdout().flush().unwrap();
--- windows_bindings/build.rs
+++ windows_bindings/build.rs
@@ -5,6 +5,6 @@ fn main() {
         Windows::Win32::Foundation::{HANDLE, HINSTANCE, MAX_PATH, CloseHandle, INVALID_HANDLE_VALUE},
         Windows::Win32::System::ProcessStatus::{K32EnumProcessModulesEx, K32GetModuleBaseNameW},
         Windows::Win32::System::Diagnostics::Debug::{ReadProcessMemory, WriteProcessMemory},
-        Windows::Win32::System::Diagnostics::ToolHelp::{CreateToolhelp32Snapshot, Module32FirstW, Module32NextW, MODULEENTRY32W},
+        Windows::Win32::System::Diagnostics::ToolHelp::{CreateToolhelp32Snapshot, Module32FirstW, Module32NextW, MODULEENTRY32W, Process32FirstW, Process32NextW, PROCESSENTRY32W},
     };
 }
