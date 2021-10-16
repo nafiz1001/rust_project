4501918053ff9056a2663e0556ec3b594b15f3c4
nafiz1001 <nafiz.islam1001@gmail.com>
2021-10-16 16:17:02 -0400

    create new process

--- linux_bindings/src/lib.rs
+++ linux_bindings/src/lib.rs
@@ -51,7 +51,9 @@ impl Iterator for ProcessIterator {
 
 #[cfg(test)]
 mod tests {
-    use crate::ProcessIterator;
+    use std::{fs::File, process::Command};
+
+    use crate::{ProcessIterator, Process};
 
     #[test]
     fn enumerate_processes() {
@@ -59,5 +61,16 @@ mod tests {
             println!("{:?}", p);
             return p
         }).count() > 0);
+    }
+
+    #[test]
+    fn new_process() {
+        let mut child = Command::new("/usr/games/moon-buggy")
+                        .spawn()
+                        .expect("failed to execute child");
+
+        Process::new(child.id());
+
+        child.kill().unwrap();
     }
 }
