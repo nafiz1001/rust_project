e89cd89683b7dffc9c8347da36338b78e068c133
nafiz1001 <nafiz.islam1001@gmail.com>
2021-12-25 15:30:40 -0500

    simplify windows and linux internal packages

--- windows/src/lib.rs
+++ windows/src/lib.rs
@@ -1,33 +1,16 @@
-#![allow(non_snake_case)]
-
-windows::include_bindings!();
-
 use std::{
     ffi::{c_void, OsString},
-    fmt,
     mem::size_of,
     ops::Range,
     os::windows::prelude::OsStringExt,
     ptr::null_mut,
 };
-
-use crate::Windows::Win32::{
-    Foundation::{CloseHandle, HANDLE, HINSTANCE, MAX_PATH},
-    System::Diagnostics::{Debug::{ReadProcessMemory, WriteProcessMemory, DebugActiveProcess, DebugActiveProcessStop}, ToolHelp::PROCESSENTRY32W},
+use windows::Win32::{
+    Foundation::*,
     System::{
-        Diagnostics::ToolHelp::{
-            CreateToolhelp32Snapshot, Module32FirstW, Module32NextW, Process32FirstW,
-            Process32NextW, MODULEENTRY32W, TH32CS_SNAPMODULE, TH32CS_SNAPMODULE32,
-            TH32CS_SNAPPROCESS,
-        },
-        Memory::{
-            VirtualQueryEx, MEMORY_BASIC_INFORMATION, MEM_COMMIT, PAGE_PROTECTION_FLAGS,
-            PAGE_READONLY, PAGE_READWRITE, PAGE_TYPE, VIRTUAL_ALLOCATION_TYPE,
-        },
-        Threading::{
-            OpenProcess, PROCESS_QUERY_INFORMATION, PROCESS_VM_OPERATION, PROCESS_VM_READ,
-            PROCESS_VM_WRITE,
-        },
+        Diagnostics::{Debug::*, ToolHelp::*},
+        Memory::*,
+        Threading::*,
     },
 };
 
@@ -36,12 +19,6 @@ fn wide_chars_to_string(wide_chars: &[u16]) -> String {
         .to_string_lossy()
         .trim_end_matches(char::from(0))
         .to_string()
-}
-
-impl fmt::Display for HANDLE {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.0)
-    }
 }
 
 pub struct ProcessEntry {
@@ -86,15 +63,7 @@ impl Iterator for ProcessIterator {
     fn next(&mut self) -> Option<Self::Item> {
         let mut process_entry = PROCESSENTRY32W {
             dwSize: size_of::<PROCESSENTRY32W>() as u32,
-            cntUsage: 0,
-            th32ProcessID: 0,
-            th32DefaultHeapID: 0,
-            th32ModuleID: 0,
-            cntThreads: 0,
-            th32ParentProcessID: 0,
-            pcPriClassBase: 0,
-            dwFlags: 0,
-            szExeFile: [0u16; MAX_PATH as usize],
+            ..Default::default()
         };
 
         unsafe {
@@ -142,15 +111,7 @@ impl Iterator for ModuleIterator {
     fn next(&mut self) -> Option<Self::Item> {
         let mut module_entry = MODULEENTRY32W {
             dwSize: size_of::<MODULEENTRY32W>() as u32,
-            th32ProcessID: 0,
-            th32ModuleID: 0,
-            GlblcntUsage: 0,
-            ProccntUsage: 0,
-            modBaseAddr: null_mut(),
-            modBaseSize: 0,
-            hModule: HINSTANCE(0),
-            szModule: [0u16; 256],
-            szExePath: [0u16; MAX_PATH as usize],
+            ..Default::default()
         };
 
         unsafe {
@@ -180,7 +141,7 @@ pub struct Process {
 
 impl Process {
     pub fn new(pid: u32) -> Self {
-        let handle: HANDLE;
+        let handle;
         unsafe {
             handle = OpenProcess(
                 PROCESS_QUERY_INFORMATION
@@ -221,7 +182,7 @@ impl Process {
                 panic!(
                     "ReadProcessMemory failed to read between the range {:#016x}..{:#016x}",
                     start,
-                    (start + buffer.len())
+                    (start + buffer.len() * size_of::<T>())
                 );
             }
         }
@@ -248,15 +209,11 @@ impl Process {
     }
 
     pub fn suspend(&self) -> bool {
-        unsafe {
-            DebugActiveProcess(self.id()).as_bool()
-        }
+        unsafe { DebugActiveProcess(self.id()).as_bool() }
     }
 
-    pub fn stop_suspend(&self) -> bool {
-        unsafe {
-            DebugActiveProcessStop(self.id()).as_bool()
-        }
+    pub fn resume(&self) -> bool {
+        unsafe { DebugActiveProcessStop(self.id()).as_bool() }
     }
 }
 
@@ -286,19 +243,13 @@ pub struct MemoryRegionIterator<'a> {
 
 impl<'a> MemoryRegionIterator<'a> {
     pub fn new(process: &'a Process, starting_address: usize) -> Self {
-        return Self {
+        Self {
             process,
             memory_basic_information: MEMORY_BASIC_INFORMATION {
                 BaseAddress: starting_address as *mut c_void,
-                AllocationBase: 0 as *mut c_void,
-                AllocationProtect: PAGE_PROTECTION_FLAGS(0),
-                PartitionId: 0,
-                RegionSize: 0,
-                State: VIRTUAL_ALLOCATION_TYPE(0),
-                Protect: PAGE_PROTECTION_FLAGS(0),
-                Type: PAGE_TYPE(0),
+                ..Default::default()
             },
-        };
+        }
     }
 }
 
@@ -328,16 +279,15 @@ impl Iterator for MemoryRegionIterator<'_> {
                     self.memory_basic_information.BaseAddress =
                         BaseAddress.offset(RegionSize as isize);
 
-                    let range = BaseAddress as usize..BaseAddress as usize + RegionSize;
                     return Some(match State {
                         MEM_COMMIT => MemoryRegionEntry {
-                            range,
+                            range: BaseAddress as usize..BaseAddress as usize + RegionSize,
                             permission: match Protect {
                                 PAGE_READONLY => MemoryPermission::READONLY,
                                 PAGE_READWRITE => MemoryPermission::READWRITE,
                                 _ => continue,
                             },
-                            info: self.process.name()
+                            info: self.process.name(),
                         },
                         _ => continue,
                     });
@@ -412,6 +362,6 @@ mod tests {
     fn suspend() {
         let process = find_process();
         assert!(process.suspend());
-        assert!(process.stop_suspend());
+        assert!(process.resume());
     }
 }
